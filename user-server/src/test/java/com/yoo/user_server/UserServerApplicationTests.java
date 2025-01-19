c51c560b626918ccf7c4dc8ca65186a0b1759efe
yoo <edel1212@naver.com>
2025-01-19 17:04:05 +0900

    user-server init

---
+++ user-server/src/test/java/com/yoo/user_server/UserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.user_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
