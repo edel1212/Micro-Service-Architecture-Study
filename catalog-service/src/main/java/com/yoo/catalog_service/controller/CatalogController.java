3496210c9b36e35023dcadd574ed993cfcc12f95
yoo <edel1212@naver.com>
2025-02-01 21:09:31 +0900

    상품 목록 API 추가

--- catalog-service/src/main/java/com/yoo/catalog_service/controller/CatalogController.java
+++ catalog-service/src/main/java/com/yoo/catalog_service/controller/CatalogController.java
@@ -1,11 +1,18 @@
 package com.yoo.catalog_service.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yoo.catalog_service.entity.CatalogEntity;
 import com.yoo.catalog_service.service.CatalogService;
+import com.yoo.catalog_service.vo.ResponseCatalog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,6 +21,7 @@ public class CatalogController {
 
     private final Environment env;
     private final CatalogService catalogService;
+    private final ObjectMapper mapper;
 
     @GetMapping("/health-check")
     public String status() {
@@ -21,6 +29,19 @@ public class CatalogController {
                 env.getProperty("local.server.port"),
                 env.getProperty("server.port"));
     }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getCatalogs() {
+        Iterable<CatalogEntity> catalogList = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+        catalogList.forEach(v -> {
+            result.add(mapper.convertValue(v, ResponseCatalog.class));
+        });
+
+        return ResponseEntity.ok(result);
+    }
+
 
 
 }
