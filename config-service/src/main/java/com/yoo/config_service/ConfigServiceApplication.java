56cc9409f5e072b68dcf113682690081ac4d1f45
yoo <edel1212@naver.com>
2025-02-13 22:21:22 +0900

    설정 파일 불러오기 성공

--- config-service/src/main/java/com/yoo/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/yoo/config_service/ConfigServiceApplication.java
@@ -2,8 +2,11 @@ package com.yoo.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// ⭐️ 해당 어노테이션을 추가하지 않으면 config 위치를 알맞게 지정해도 찾을 수 없음
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
