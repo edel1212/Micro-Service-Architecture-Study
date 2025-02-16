e86d6bd77ff2ab03e589e6bb3899c88b2692f21e
yoo <edel1212@naver.com>
2025-02-16 15:58:49 +0900

    config-server 값 가져오기 적용

--- api-gateway/gateway-server/src/main/java/com/yoo/gateway_server/GatewayServerApplication.java
+++ api-gateway/gateway-server/src/main/java/com/yoo/gateway_server/GatewayServerApplication.java
@@ -1,7 +1,9 @@
 package com.yoo.gateway_server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
@@ -10,4 +12,11 @@ public class GatewayServerApplication {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
+	/**
+	 * ✅ 연결되어 있는 Micro Service의 상태를 받아 올 수 있게 끔함
+	 * */
+	@Bean
+	public InMemoryHttpExchangeRepository httpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
 }
