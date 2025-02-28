293f5e83e6137a626ace7df7b7b99484b0dac176
yoo <edel1212@naver.com>
2025-02-28 21:33:48 +0900

    @LoadBalanced 적용

--- user-service/src/main/java/com/yoo/user_service/config/AppConfig.java
+++ user-service/src/main/java/com/yoo/user_service/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.yoo.user_service.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,10 @@ import java.time.Duration;
 
 @Configuration
 public class AppConfig {
+
+    // ⭐️ 해당 어노테이션 사용 시 application.yml에서 불러오는 도메인 주소가 Eureka에 등록된 값으로 불러올 수 있음
+    //   - 단 Gateway를 거치지 않음
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         int TIMEOUT = 5000;
