f29c7364bea23feee4410d00551b005dd3fb5456
yoo <edel1212@naver.com>
2025-03-11 21:00:24 +0900

    Kafka Connect Sink 사용  producer 구현

---
+++ order-service/src/main/java/com/yoo/order_service/messagequeue/KafkaOrderProducer.java
@@ -0,0 +1,60 @@
+package com.yoo.order_service.messagequeue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yoo.order_service.dto.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class KafkaOrderProducer {
+    private final ObjectMapper objectMapper;
+    private final KafkaTemplate kafkaTemplate;
+
+    // fields, schema 필드는 고정 값
+    List<Field> fields = Arrays.asList(new Field("string", true, "order_id"),
+            new Field("string", true, "user_id"),
+            new Field("string", true, "product_id"),
+            new Field("int32", true, "qty"),
+            new Field("int32", true, "unit_price"),
+            new Field("int32", true, "total_price"));
+    Schema schema = Schema.builder()
+            .type("struct")
+            .fields(fields)
+            .optional(false)
+            .name("orders")
+            .build();
+
+    public OrderDto send(String topic, OrderDto orderDto) {
+        Payload payload = Payload.builder()
+                .order_id(orderDto.getOrderId())
+                .user_id(orderDto.getUserId())
+                .product_id(orderDto.getProductId())
+                .qty(orderDto.getQty())
+                .unit_price(orderDto.getUnitPrice())
+                .total_price(orderDto.getTotalPrice())
+                .build();
+
+        KafkaOrderDto kafkaOrderDto = new KafkaOrderDto(schema, payload);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = "";
+        try {
+            jsonInString = mapper.writeValueAsString(kafkaOrderDto);
+        } catch(JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+
+        kafkaTemplate.send(topic, jsonInString);
+        log.info("Order Producer sent data from the Order microservice: " + kafkaOrderDto);
+
+        return orderDto;
+    }
+}
