3ba502a141f81f30519af91508d9473f76c24ecc
yoo <edel1212@naver.com>
2025-03-30 19:55:50 +0900

    catalog service add compose

--- catalog-service/src/main/java/com/yoo/catalog_service/config/KafkaConsumerConfig.java
+++ catalog-service/src/main/java/com/yoo/catalog_service/config/KafkaConsumerConfig.java
@@ -1,9 +1,12 @@
 package com.yoo.catalog_service.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -16,11 +19,21 @@ import java.util.Map;
 // -  Kafka 리스너를 자동으로 감지하고 관리할 수 있도록 설정하는 역할을 함
 @EnableKafka
 @Configuration
+@RequiredArgsConstructor
+@Log4j2
 public class KafkaConsumerConfig {
+    private final Environment env;
     @Bean
     public ConsumerFactory<String, String> consumerFactory(){
+
+        String kafka = env.getProperty("kafka.url","127.0.0.1:9092");
+
+        log.info("---------------------");
+        log.info("kafka :: {}",kafka);
+        log.info("---------------------");
+
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
